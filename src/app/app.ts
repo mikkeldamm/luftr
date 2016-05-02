27ef1b7101e76fb834544217ae761ba1c3360c2b
mikkeldamm <mikkeldamm@hotmail.com>
2016-05-03 01:15:46 +0200

    created example for modal for use in login and register flow. Only login applied

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,14 @@
-import {Component, Directive, ElementRef, Renderer, ViewEncapsulation, HostListener} from 'angular2/core';
-import {RouteConfig, Router, ROUTER_DIRECTIVES} from 'angular2/router';
+import {
+        Injector, 
+        Component, 
+        ElementRef, 
+        Renderer, 
+        ViewEncapsulation, 
+        HostListener, 
+        DynamicComponentLoader,
+        ComponentRef
+} from 'angular2/core';
+import {RouteConfig, Router, AuxRoute, ROUTER_DIRECTIVES} from 'angular2/router';
 
 import {Hero} from './components/hero/hero';
 import {Logo} from './components/logo/logo';
@@ -25,24 +34,74 @@ import {Profile} from './components/profile/profile';
 })
 @RouteConfig([
     { path: '/', component: Home, name: 'Home', useAsDefault: true },
-    { path: '/login', component: Login, name: 'Login' },
+    { path: '/login', component: Home, name: 'Login' },
     { path: '/login/oauth', component: Login, name: 'LoginAuth' },
-    { path: '/register', component: RegisterUser, name: 'RegisterUser' },
+    { path: '/register', component: Home, name: 'RegisterUser' },
     { path: '/profile/...', component: Profile, name: 'Profile' }
 ])
 export class App {
     
-    shouldShowScene: boolean = false;
+    shouldShowScene: boolean = true;
     isScrolledDown: boolean = false;
     
-    constructor(public router: Router) {
+    isModalActive: boolean = false;
+    componentReference: ComponentRef;
+    
+    constructor(
+        private _router: Router,
+        private _dynamicComponentLoader: DynamicComponentLoader,
+        private _elementRef: ElementRef,
+        private _injector: Injector
+        ) {
         
-        router.subscribe((url) => {
+        _router.subscribe((url) => {
             
-            this.shouldShowScene = url === "";
+            //this.shouldShowScene = url === "";
+            
+            if (url === "login") {
+                
+                this.closeModal();
+                this.loadLoginComponent();
+                
+            } else if (url === "register") {
+                
+                this.closeModal();
+                this.loadRegisterComponent();
+            }
         });
     }
     
+    closeModal() {
+        
+        if (this.componentReference) {
+            this.componentReference.dispose();
+            this.isModalActive = false;
+        }
+    }
+    
+    private loadLoginComponent() {
+        
+        var componentPromise = this._dynamicComponentLoader.loadIntoLocation(Login, this._elementRef, 'modal');
+        
+        componentPromise.then((comRef) => {
+            
+            this.isModalActive = true;
+            this.componentReference = comRef;
+        });
+    }
+    
+    private loadRegisterComponent() {
+        
+        var componentPromise = this._dynamicComponentLoader.loadIntoLocation(RegisterUser, this._elementRef, 'modal');
+        
+        componentPromise.then((comRef) => {
+            
+            this.isModalActive = true;
+            this.componentReference = comRef;
+        });
+    }
+    
+    // TODO: make this a directive instead
     @HostListener('window:scroll', ['$event']) 
     handleScrollEvent(e) {
         
