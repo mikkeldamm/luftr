15c537ea53ec6959057661d621fb320a2490ee56
mikkeldamm <mikkeldamm@hotmail.com>
2016-05-25 00:09:33 +0200

    added methods to get jwt, idtoken and accesstoken from authstate service

--- src/app/auth/authState.ts
+++ src/app/auth/authState.ts
@@ -2,6 +2,23 @@ import {Provider} from '@angular/core';
 
 export class AuthState {
     
+    private _data: AuthStorage;
+    
+    get accessToken(): string {
+        
+        return this.Data.accessToken;
+    }
+    
+    get idToken(): string {
+        
+        return this.Data.idToken;
+    }
+    
+    get jwtToken(): string {
+        
+        return this.Data.jwt;
+    }
+    
     isAuthenticated(): boolean {
         
         let authData: AuthStorage = localStorage.getItem("luftr_auth");
@@ -11,18 +28,31 @@ export class AuthState {
     
     setAuthenticated(token: string, idToken: string, jwt: string): void {
         
-        let data = <AuthStorage>{
+        this._data = <AuthStorage>{
             accessToken: token,
             idToken: idToken,
             jwt: jwt
         };
         
-        localStorage.setItem("luftr_auth", JSON.stringify(data));
+        localStorage.setItem("luftr_auth", JSON.stringify(this._data));
     }
     
     setNotAuthenticated(): void {
         
         localStorage.removeItem("luftr_auth");
+    }
+    
+    private get Data() {
+        
+        if (!this._data)
+            this._data = this.getAuthStorage();
+            
+        return this._data;
+    }
+    
+    private getAuthStorage(): AuthStorage {
+        
+        return <AuthStorage>JSON.parse(localStorage.getItem("luftr_auth"));
     }
 }
 
